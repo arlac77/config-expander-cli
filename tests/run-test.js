f744ce303eef5f8fe33f9318073b404f641bd0f8
Markus Felten <Markus.Felten@gmx.de>
2018-06-30 21:10:10 +0200

    docs: sync API

--- tests/run-test.js
+++ tests/run-test.js
@@ -2,7 +2,7 @@ import test from 'ava';
 import { join } from 'path';
 import execa from 'execa';
 
-test('rtsp-archive', async t => {
+test('config-expander', async t => {
   const result = await execa(join(__dirname, '..', 'bin', 'config-expander'), [
     '-h'
   ]);
