5347aa21c412ecb55714d4f2c010ab7af21e8f84
Markus Felten <Markus.Felten@gmx.de>
2018-09-28 23:34:41 +0200

    chore(rollup): use compact of rollup-plugin-json

--- rollup.config.js
+++ rollup.config.js
@@ -5,19 +5,26 @@ import executable from "rollup-plugin-executable";
 import json from "rollup-plugin-json";
 import pkg from "./package.json";
 
-export default [
-  ...Object.keys(pkg.bin || {}).map(name => {
-    return {
-      input: `src/${name}.mjs`,
-      output: {
-        file: pkg.bin[name],
-        format: "cjs",
-        banner:
-          "#!/usr/bin/env -S node --experimental-modules --experimental-worker",
-        interop: false
-      },
-      plugins: [resolve(), commonjs(), json(), executable()],
-      external: ["os", "path", "config-expander"]
-    };
-  })
-];
+export default Object.keys(pkg.bin || {}).map(name => {
+  return {
+    input: `src/${name}.mjs`,
+    output: {
+      file: pkg.bin[name],
+      format: "cjs",
+      banner:
+        "#!/usr/bin/env -S node --experimental-modules --experimental-worker",
+      interop: false
+    },
+    plugins: [
+      resolve(),
+      commonjs(),
+      json({
+        include: "package.json",
+        preferConst: true,
+        compact: true
+      }),
+      executable()
+    ],
+    external: ["os", "path", "config-expander"]
+  };
+});
