03e66b5fc2c7e89351161cb8403fa68b527d6fcc
Markus Felten <Markus.Felten@gmx.de>
2019-07-13 15:35:00 +0200

    fix: arg passing

--- src/config-expander-cli.mjs
+++ src/config-expander-cli.mjs
@@ -1,36 +1,25 @@
 import { expand } from "config-expander";
 import { version, description } from "../package.json";
-import { dirname } from "path";
 import program from "commander";
 
-const configValues = [];
+const constants = {};
 
 program
   .version(version)
   .description(description)
-  .command("expand", "expand config")
-  .option("-d --define <key=value>", "define (config) value", value =>
-    configValues.push(value)
-  )
-  .action(async (args) => {
-
-    const constants = {
-      basedir: dirname(args || process.cwd())
-    };
-
-    configValues.forEach(value => {
-      const m = value.match(/^([a-zA-Z_][a-zA-Z_0-9]*)=(.*)/);
-      if (m) {
-        constants[m[1]] = m[2];
-      }
-    });
-
-    const config = await expand(
-      args.config ? "${include('" + args + "')}" : {},
+  .option("-d --define <key=value>", "define (config) value", value => {
+    const m = value.match(/^([a-zA-Z_][a-zA-Z_0-9]*)=(.*)/);
+    if (m) {
+      constants[m[1]] = m[2];
+    }
+  })
+  .action(async args => {
+    const config = await expand("${include('" + args + "')}",
       {
         constants
       }
     );
 
     process.stdout.write(JSON.stringify(config, undefined, 2));
-  }).parse(process.argv);
+  })
+  .parse(process.argv);
--- tests/run-test.mjs
+++ tests/run-test.mjs
@@ -1,41 +1,37 @@
-import test from 'ava';
-import { join } from 'path';
-import execa from 'execa';
+import test from "ava";
+import execa from "execa";
 import { join, dirname } from "path";
 import { fileURLToPath } from "url";
 
 const here = dirname(fileURLToPath(import.meta.url));
 
-test('config-expander -h', async t => {
-  const result = await execa(join(here, '..', 'bin', 'config-expander'), [
-    '-h'
+test("config-expander -h", async t => {
+  const result = await execa(join(here, "..", "bin", "config-expander"), [
+    "-h"
   ]);
-  t.regex(result.stdout, /config-expander expand/);
+  t.regex(result.stdout, /Usage: config-expander/);
 });
 
-test('config-expander', async t => {
-  const result = await execa(join(here, '..', 'bin', 'config-expander'), [
-    'expand',
-    join(here, '..', 'tests', 'fixtures', 'config.json')
+test("config-expander", async t => {
+  const result = await execa(join(here, "..", "bin", "config-expander"), [
+    join(here, "..", "tests", "fixtures", "config.json")
   ]);
   t.regex(result.stdout, /"a":\s*1/);
 });
 
-test('config-expander constant', async t => {
-  const result = await execa(join(here, '..', 'bin', 'config-expander'), [
-    'expand',
-    join(here, '..', 'tests', 'fixtures', 'config.json')
+test("config-expander constant", async t => {
+  const result = await execa(join(here, "..", "bin", "config-expander"), [
+    join(here, "..", "tests", "fixtures", "config.json")
   ]);
   t.regex(result.stdout, /"b":\s*77/);
   t.regex(result.stdout, /"c":\s*77/);
 });
 
-test('config-expander -d', async t => {
-  const result = await execa(join(here, '..', 'bin', 'config-expander'), [
-    'expand',
-    '-d',
-    'c2=88',
-    join(here, '..', 'tests', 'fixtures', 'config.json')
+test("config-expander -d", async t => {
+  const result = await execa(join(here, "..", "bin", "config-expander"), [
+    "-d",
+    "c2=88",
+    join(here, "..", "tests", "fixtures", "config.json")
   ]);
   t.regex(result.stdout, /"d":\s*"88"/);
 });
